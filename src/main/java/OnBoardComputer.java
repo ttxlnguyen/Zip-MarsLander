ce416f4a4ab428080dfb3d96bb1bf46e2511fd7c
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-07-31 00:18:24 -0400

    finished, implemented given hint to OnBoardComputer, both methods in the class work, first method commented out

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -1,19 +1,37 @@
 public class OnBoardComputer implements BurnStream {
 
+//    @Override
+//    public int getNextBurn(DescentEvent status) {
+//        int burn = 0;
+//
+//        if (status.getVelocity() == 2) {
+//            burn = 100;
+//        } else if (status.getAltitude() > 6000) {
+//            burn = 100;
+//        } else if (status.getVelocity() > 100) {
+//            burn = 200;
+//        } else if (status.getAltitude() > 100) {
+//            burn = 100;
+//        } else {
+//            burn = 198;
+//        }
+//
+//        System.out.println(burn); /*hack!*/
+//        return burn;
+//    }
+
     @Override
     public int getNextBurn(DescentEvent status) {
         int burn = 0;
 
-        if (status.getVelocity() == 2) {
-            burn = 100;
-        } else if (status.getAltitude() > 6000) {
-            burn = 100;
-        } else if (status.getVelocity() > 100) {
-            burn = 200;
-        } else if (status.getAltitude() > 100) {
-            burn = 100;
+        if (status.Altitude > 1){
+            int acceleration = (status.Velocity * status.Velocity) / (2 * status.Altitude);
+            burn = acceleration + 100;
+            if (burn > 200){
+                burn = 200;
+            }
         } else {
-            burn = 198;
+            burn = 100;
         }
 
         System.out.println(burn); /*hack!*/
